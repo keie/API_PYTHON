317c979e735876c7cbc3457ae92f21f956174f56
OswalLopezM <oswaldo7365@hotmail.com>
2020-02-27 20:13:42 -0400

    cors acomodado

--- restfulapicrud/settings.py
+++ restfulapicrud/settings.py
@@ -39,18 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'employeeapi',
     'fileapi',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL=True
+
 
 ROOT_URLCONF = 'restfulapicrud.urls'
 
