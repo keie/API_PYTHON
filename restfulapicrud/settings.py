fece81fd42dce7c33fce374e89eb8f56af94c44e
OswalLopezM <oswaldo7365@hotmail.com>
2020-02-29 10:52:33 -0400

    setting con cors

--- restfulapicrud/settings.py
+++ restfulapicrud/settings.py
@@ -39,19 +39,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'employeeapi',
     'fileapi',
+    'corsheaders',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL=True
 ROOT_URLCONF = 'restfulapicrud.urls'
 
 TEMPLATES = [
@@ -81,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'restfulapiDB',
         'USER':'postgres',
-        'PASSWORD':'root',
+        'PASSWORD':'Peliculas1',
         'HOST':'localhost'
     }
 }
